f37fc5878f68460de0013efa43717689b2230778
Igor Matsunaga <38197530+igorMatsunaga@users.noreply.github.com>
2019-06-16 16:58:44 -0300

    Update batPonto.py

--- batPonto.py
+++ batPonto.py
@@ -13,9 +13,9 @@ td3 = '2018-04-30 16:35:01.804925'
 
 def batPonto():
     # Criar Instancia
-    driver = webdriver.Chrome('C:\\Users\\igortomio\\Desktop\\backup\\chromedriver_win32//chromedriver')
+    driver = webdriver.Chrome('C:\\Users\\igor\\Desktop\\backup\\chromedriver_win32//chromedriver')
     # Abrir Pagina
-    driver.get('http://srv-vibpan01:8082/sistema/login.xhtml')
+    driver.get('http://srv-vib01:8082/sistema/login.xhtml')
     # Maximizar janela
     driver.maximize_window()
     # Instancia formularios
@@ -27,7 +27,7 @@ def batPonto():
     # aperta botão
     elem.click()
     # Redireciona Pagina
-    driver.get('http://srv-vibpan01:8082/sistema/secured/Registro.xhtml')
+    driver.get('http://srv-vib01:8082/sistema/secured/Registro.xhtml')
     # Instancia botão
     btn = driver.find_element_by_xpath('//*[@id="botaoSalvar"]/span')
     btn.click()
